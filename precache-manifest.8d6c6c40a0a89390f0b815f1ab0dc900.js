2225bde545d3935f836148dc8680fbed61e4270e
PKDankyi <paadankyi@gmail.com>
2020-07-17 23:34:10 +0000

    Updates

---
+++ precache-manifest.8d6c6c40a0a89390f0b815f1ab0dc900.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82ddb7a2feae1f52499160b4f4575231",
+    "url": "/HostedCovidApp/index.html"
+  },
+  {
+    "revision": "3bcf826b40db740ed8d4",
+    "url": "/HostedCovidApp/static/css/main.b5f59e31.chunk.css"
+  },
+  {
+    "revision": "0d89e03eb404fb18c6fa",
+    "url": "/HostedCovidApp/static/js/2.a07642e0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/HostedCovidApp/static/js/2.a07642e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bcf826b40db740ed8d4",
+    "url": "/HostedCovidApp/static/js/main.f289f231.chunk.js"
+  },
+  {
+    "revision": "21ce1069d79c01c0a3b5",
+    "url": "/HostedCovidApp/static/js/runtime-main.e3dbf09e.js"
+  }
+]);
